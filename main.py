5c6fedd92db7fb06421eb64bebc2fb99a55bb73e
YutoHiruta <r202170058pd@jindai.jp>
2021-08-26 13:29:59 +0900

    Create ML Environment.

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 import math
 import argparse
-from rulemodel import *
 import datetime
+
+#自前環境
+from rulemodel import *
 from print_message import *
+from envs.rulemodel_env import rulemodel_env
+
+#gym関連
+import gym
+from gym.envs.registration import register
+from tensorflow import keras
+from rl.agents.dqn import DQNAgent
+from rl.policy import BoltzmannQPolicy
+from rl.memory import SequentialMemory
+
+#グラフ出力
+import matplotlib.pyplot as plt
+
+
 
 parser = argparse.ArgumentParser()
 
@@ -11,6 +27,30 @@ parser.add_argument(
     type=str,
     help="読み込むルールファイルのパス. ClassBenchルール変換プログラムの6番を使用し,assign_evaluation_to_rulelist.pyで評価型を付与すること.(使用せずに評価型付与の番号で作れたならそれでヨシ)")
 
+parser.add_argument(
+    "--packets",
+    type=str,
+    default=None,
+    help="読み込むパケットファイルのパス.ClassBenchルール変換プログラムの6番を使用すること.無指定の場合は一様分布(全ての場合のパケット1つずつ).")
+
+
+#うるせぇアルパカぶつけるぞ 
+#Δ~~~~Δ 
+#ξ ･ェ･ ξ 
+#ξ　~　ξ 
+#ξ　　 ξ 
+#ξ　　　“~～~～〇 
+#ξ　　　　　　 ξ 
+#ξ　ξ　ξ~～~ξ　ξ 
+#　ξ_ξξ_ξ　ξ_ξξ_ξ 
+#　　ヽ(´･ω･)ﾉ 
+#　　　 |　 / 
+#　　　 UU"
+
+
+#
+# -- メイン処理 --
+#
 if __name__ == "__main__":
 
     args = parser.parse_args()
@@ -25,12 +65,113 @@ if __name__ == "__main__":
                 break
             rule_list.append(Rule(rule[0],rule[1]))
 
+    #パケットリストを形成
+    packet_list = []
+    if args.packets != None:
+        with open(args.packets,mode="r") as packetlist_file:
+            while packetlist_file:
+                packet = "".join(packetlist_file.readline().split())
+                #print(packet)
+                if not packet:
+                    break
+                packet_list.append(packet)
+    else:
+        max_num = 2**len(rule_list[0].bit_string)
+        specifier = "0"+str(len(rule_list[0].bit_string))+"b"
+        for i in range(max_num):
+            packet_list.append(format(i,specifier))
 
+
+
+    #================================================================
+    #=                       機械学習をする部分                        =
+    #================================================================
+    #参考にしたサイト : https://qiita.com/mahoutsukaino-deshi/items/54a30218a57c88798c17
+    
+    #gymに環境を登録し、初期化変数を設定
+    register(
+        id='rulelistRecontrust-v0',
+        entry_point='envs.rulemodel_env:rulemodel_env',
+        kwargs={
+            'rulelist':rule_list,
+            'packetlist':packet_list,
+            'max_steps':200,
+            'max_stay':10
+        },
+    )
+
+    #環境呼び出し
+    env = gym.make('rulelistRecontrust-v0')
+
+    #環境初期化
+    env._reset()
+
+    #Kerasによるニューラルネットワークモデル作成
+    nb_actions = env.action_space_size
+    print(nb_actions)
+    model = keras.models.Sequential([
+        keras.layers.Flatten(input_shape=(1,) + env.observation_space.shape),
+        keras.layers.Dense(64,activation="relu"),
+        keras.layers.Dense(64,activation="relu"),
+        keras.layers.Dense(64,activation="relu"),
+        keras.layers.Dense(nb_actions,activation="linear"),
+    ])
+
+    model.summary()
+    #経験蓄積メモリの定義
+    memory = SequentialMemory(limit=50000, window_length=1)
+    #ポリシの選択
+    policy = BoltzmannQPolicy()
+    #DQNAgent作成
+    dqn = DQNAgent(
+        model=model,
+        nb_actions=nb_actions,
+        memory=memory,
+        target_model_update=1e-2,
+        policy=policy
+    )
+    #DQNAgentのコンパイル
+    dqn.compile(keras.optimizers.Adam(lr=1e-4),metrics=['mae'])
+
+    #学習開始
+    history = dqn.fit(env,nb_steps=50000,visualize=False, verbose=1)
+
+    #学習した重みを保存
+    dqn.save_weights('result.hdf5')
+
+    #グラフ化
+    plt.plot(history.history['nb_episode_steps'], label='nb_episode_steps')
+    plt.legend()
+    plt.show()
+    plt.plot(history.history['episode_reward'], label='episode_reward')
+    plt.legend()
+    plt.show()
+    
+
+    
+    """
+    #==============ランダムなアクションを環境に適用するテスト==============
+    for i in range(100):
+        #ランダムなアクションを呼び出し
+        action = env.action_space.sample()
+
+        #1ステップ実行してパラメータを取得
+        observation, reward, done, _ = env._step(action)
+
+        #アクションと報酬を出力
+        print(action,end="")
+        print(reward)
+    #===============================================================
+    """
+
+    
+    """
     # -- メニュー表示 -- 
     Print_Message.print_menu(rule_list)
 
     except_input = False
 
+    
     # -- メニュー項目選択 --
     while(not except_input):
         x = input(" >> ")
@@ -46,3 +187,4 @@ if __name__ == "__main__":
             exit()
         else:
             Print_Message.print_error_unexpect()
+    """

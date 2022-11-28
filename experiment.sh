4bf3bb6c70aff27586647dbcc0e89139ebfcd355
YutoHiruta <r202170058pd@jindai.jp>
2022-11-28 17:38:38 +0900

    実験shellに引数num_envを追加

--- experiment.sh
+++ experiment.sh
@@ -24,7 +24,7 @@ do
 	python3 run_heuristics.py Datas/$1/Rule/sample$counter SGM --packets=Datas/$1/Packet/sample$counter --experiment_title=$1 --sample_number=$counter
 	python3 run_heuristics.py Datas/$1/Rule/sample$counter Hikage --packets=Datas/$1/Packet/sample$counter --experiment_title=$1 --sample_number=$counter
 	python3 dump_dependency_graph.py Datas/$1/Rule/sample$counter --packets=Datas/$1/Packet/sample$counter --figsize=50
-	python run_neuroreorder.py Datas/$1/Rule/sample$counter --packets=Datas/$1/Packet/sample$counter --experiment_title=$1 --sample_number=$counter --max_steps=$3
+	python run_neuroreorder.py Datas/$1/Rule/sample$counter --packets=Datas/$1/Packet/sample$counter --experiment_title=$1 --sample_number=$counter --max_steps=$3 --num_env=12
 	mv Dump/sample$counter Dump/$1/
 	mv Dump/sample$counter.png Dump/$1/sample$counter/dependency_graph.png
 	echo "サンプル $counter 処理終了"
